eb7d509d74f312cc0590d0170779eddbcd6ccf47
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-07-17 19:19:37 +0200

    feat(init): init backend ecommerce

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail("test@test.com")
+          ->setPassword($this->hasher->hashPassword($user, "test"))
+          ->setRoles(["ROLE_ADMIN"]);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
