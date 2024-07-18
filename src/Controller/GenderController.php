c508ea5d8b64afb2457003484e4b2be406412277
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-07-18 14:20:36 +0200

    feat(Controller): add controller gender
    Add find all
    Add create gender
    
    Migration bdd creatAt updateAt gender entity

---
+++ src/Controller/GenderController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Gender;
+use App\Form\GenderType;
+use App\Form\Traits\ApiControllerTrait;
+use App\Repository\GenderRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GenderController extends AbstractController
+{
+    use ApiControllerTrait;
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private GenderRepository $repo,
+    ) {
+
+    }
+    #[Route('/api/gender', name: 'app_gender')]
+    public function index(): JsonResponse
+    {
+        $gender = $this->repo->findAll();
+        return $this->json($gender);
+    }
+
+    #[Route('/api/gender/create', name: 'app_gender_create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $gender = new Gender();
+        $form = this->createForm(GenderType::class, $gender, [csrf_protection => false]);
+        $data = json_decode($request->getContent(), true);
+
+        $form->submit($data, false);
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($gender);
+            $this->em->flush();
+            return $this->json($gender, 201);
+        }
+        return $this->getFormErrors($form);
+    }
+}
